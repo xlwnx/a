692e9c7ca3184742ae61ae5da3bfb104065dc29c
xlwnx <alwanfarid19@gmail.com>
2026-04-24 21:13:04 +0700

    first commit

---
+++ twrp_m12.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. We will overwrite some of these property like name, model, etc.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_DEVICE := m12
+PRODUCT_NAME := twrp_m12
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M127F
+PRODUCT_MANUFACTURER := samsung
